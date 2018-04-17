7080341514c3036b4e24f2fc73ce9d43952ee451
Michal Krzymiński <michal.krzyminski@onwelo.com>
2018-04-17 16:41:35 +0200

    class part 1

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -3,6 +3,10 @@ package com.timbuchalka;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Car porsche = new Car();
+        Car holden = new Car();
+
+        porsche.setModel("911");
+        System.out.println(porsche.getModel());
     }
 }
